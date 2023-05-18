d6f1a9f34a29098d2ec2dd37e92cd430398c861c
Eleonora <norka2233@gmail.com>
2023-05-18 14:38:14 +0300

    created database with SQLAlchemy

--- app/models.py
+++ app/models.py
@@ -1,11 +1,23 @@
+from datetime import datetime
 from app import db
 
 
 class DogUser(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    dogname = db.Column(db.String(64), index=True, unique=True)
+    dog_name = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
+    posts = db.relationship('Post', backref='author', lazy='dynamic')
 
     def __repr__(self):
-        return f'<Dog User {self.dogname}>'
+        return f'<Dog User {self.dog_name}>'
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String(140))
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    dog_user_id = db.Column(db.Integer, db.ForeignKey('dog_user.id'))
+
+    def __repr__(self):
+        return f'<Post {self.body}>'
