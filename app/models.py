79bcadcc713608eb757c76753686c142de934c61
Eleonora <norka2233@gmail.com>
2023-05-19 18:07:36 +0300

    login functionality

--- app/models.py
+++ app/models.py
@@ -1,8 +1,11 @@
 from datetime import datetime
 from app import db
+from app import login
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
 
 
-class DogUser(db.Model):
+class DogUser(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     dog_name = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
@@ -11,6 +14,17 @@ class DogUser(db.Model):
 
     def __repr__(self):
         return f'<Dog User {self.dog_name}>'
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+
+@login.user_loader
+def load_dog_user(id):
+    return DogUser.query.get(int(id))
 
 
 class Post(db.Model):
