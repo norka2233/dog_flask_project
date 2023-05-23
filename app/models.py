84f78209ab1607e44062c80c982655ab67d108a5
Eleonora <norka2233@gmail.com>
2023-05-23 17:38:27 +0300

    profile page

--- app/models.py
+++ app/models.py
@@ -1,8 +1,9 @@
 from datetime import datetime
+from hashlib import md5
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
 from app import db
 from app import login
-from werkzeug.security import generate_password_hash, check_password_hash
-from flask_login import UserMixin
 
 
 class DogUser(UserMixin, db.Model):
@@ -11,6 +12,8 @@ class DogUser(UserMixin, db.Model):
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
     posts = db.relationship('Post', backref='author', lazy='dynamic')
+    about_me = db.Column(db.String(140))
+    last_seen = db.Column(db.DateTime, default=datetime.utcnow)
 
     def __repr__(self):
         return f'<Dog User {self.dog_name}>'
@@ -20,6 +23,10 @@ class DogUser(UserMixin, db.Model):
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+
+    def avatar(self, size):
+        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+        return f'https://www.gravatar.com/avatar/{digest}?d=identicon&s={size}'
 
 
 @login.user_loader
